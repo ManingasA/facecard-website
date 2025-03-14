0b4ba4117d240b240cfd35d358b15efc7824777d
ManingasA <arnelmnngs899@gmail.com>
2025-03-14 15:38:17 +0800

    MainPage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
  /** @type {import('tailwindcss').Config} */
  export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        elegant: ["Playfair Display", "serif"],
+      },
+    },
   },
   plugins: [],
 }
